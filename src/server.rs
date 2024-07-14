75264f46791df4ab381d4752b69a53ae5d6c3c6f
Adam Getchell <acgetchell@ucdavis.edu>
2024-07-13 21:37:25 -0700

    Initial commit

---
+++ src/server.rs
@@ -0,0 +1,20 @@
+use leptos_spin::{render_best_match_to_stream, RouteTable, server_fn::register_explicit};
+use spin_sdk::http::{IncomingRequest, ResponseOutparam};
+use spin_sdk::http_component;
+
+#[http_component]
+async fn handle_spin_leptos(req: IncomingRequest, resp_out: ResponseOutparam) {
+    let mut conf = leptos::get_configuration(None).await.unwrap();
+    // conf.leptos_options.output_name = "spin_leptos".to_owned();
+    conf.leptos_options.output_name = "spin_leptos".into();
+
+    // Register server functions
+    register_explicit::<crate::app::SaveCount>();
+
+    let app = crate::app::App;
+
+    let mut routes = RouteTable::build(app);
+    routes.add_server_fn_prefix("/api").unwrap();
+
+    render_best_match_to_stream(req, resp_out, &routes, app, &conf.leptos_options).await
+}
